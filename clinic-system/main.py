e454f2806a401cc67c3da896952bd34ec8caa74c
mpoleli <149790742+mpoleli@users.noreply.github.com>
2026-05-07 15:05:49 +1400

    Update main.py

--- clinic-system/main.py
+++ clinic-system/main.py
@@ -110,5 +110,4 @@ def login():
 
 # ================= RUN (RENDER FIX) =================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
